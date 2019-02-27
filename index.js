ba9378ab085516fda6815eb86755e5b7dbd71eee
Julia <juliaaugust96@gmail.com>
2019-02-27 06:04:44 +0300

    Work with database

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+const app = require('./app');
+const database = require('./database');
+const config = require('./config');
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+database()
+  .then(info => {
+    console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+    app.listen(config.PORT, () =>
+      console.log(`Example app listening on port ${config.PORT}!`)
+    );
+  })
+  .catch(() => {
+    console.error('Unable to connect to database');
+    process.exit(1);
+  });
 
-const arr = ["First", "Second", "Third"];
-
-app.get('/', function (req, res) {
-  res.render("index.ejs", {arr: arr});
-});
-
-app.get('/create', function (req, res) {
-  res.render("create");
-});
-
-app.post('/create', function (req, res) {
-  arr.push(req.body.text);
-  res.redirect("/");
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
